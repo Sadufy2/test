04a3ff11bc968ec0373de8169f241f7ec7dfc4e0
Sadufy <sadufy2@gmail.com>
2023-05-02 17:58:26 +0200

    blink

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 import time
 import board
 import neopixel
+import random
 
 npixel = 6
 strip = neopixel.NeoPixel(board.D21, npixel)
@@ -9,5 +10,17 @@ speed = 0.1
 
 off = (0, 0, 0)
 
-strip[0] = (0, 255, 0)
-time.sleep(3)
+def Blink(idx):
+    for i in range(0, 30):
+        strip[idx] = (0, 0, 100)
+        time.sleep(random.randint(1, 10)/10)
+        strip[idx] = off
+        time.sleep(random.randint(1, 10)/10)
+
+t0 = threading.Thread(target=Blink, args=(0,))
+t1 = threading.Thread(target=Blink, args=(1,))
+
+t0.start()
+t1.start()
+
+strip.fill(off)
